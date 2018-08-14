c12158d701d0201293c82035adeb60dea1f6f927
9112Michael <sitkabikemike@yahoo.com>
2018-08-14 07:21:22 -0700

    Changes required by review

--- frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
+++ frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
@@ -14,19 +14,16 @@ $( () => {
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', () => {
-        /* This is our first test - it tests to make sure that the
+        /* This test makes sure that the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * empty.
          */
         it('allFeeds variable is defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* This test loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -37,7 +34,7 @@ $( () => {
             }
         });  
 
-        /* TODO: Write a test that loops through each feed
+        /* This test loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -48,25 +45,21 @@ $( () => {
             }
         });
     });
-        /* TODO: Write a new test suite named "The menu" */
+        /* This is a new test suite named "The menu" */
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
     describe('The menu', () => {
         const menu = document.querySelector('body');
+         /* This test ensures the menu element is
+         * hidden by default.
+         */
         it('menu is hidden by default', () => {
             expect (menu.classList.contains('menu-hidden')).toBe(true);
         });
-
+        /* This test ensures the menu changes
+          * visibility when the menu icon is clicked. This test
+          * has two expectations: does the menu display when
+          * clicked and does it hide when clicked again.
+          */
         it('menu toggles on and off', () => {
             const menuIcon = document.querySelector('.menu-icon-link');
 
@@ -77,30 +70,24 @@ $( () => {
             expect(menu.classList.contains('menu-hidden')).toBe(true);
         });
     });
-        /* TODO: Write a new test suite named "Initial Entries" */
-        /* TODO: Write a test that ensures when the loadFeed
+        /* This is a new test suite named "Initial Entries" */
+      
+    describe('Initial Entries', () => {
+        beforeEach((done) => {
+            loadFeed(0, done);
+        });
+        /* This test ensures that when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
-    describe('Initial Entries', () => {
-        beforeEach((done) => {
-            loadFeed(0, done);
-        });
-
         it('ensures at least one entry is available', () => {
-            const entries = document.querySelector('.feed');
-            expect(entries.children.length > 0).toBe(true);
+            const entries = document.querySelector('.feed').querySelectorAll;
+            expect(entries.length > 0).toBe(true);
         });
     });
-         /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+         /* This is a new test suite named "New Feed Selection" */
 
     describe('New Feed Selection', () => {
         let initURL;
@@ -109,17 +96,21 @@ $( () => {
         beforeEach((done) => {
             const feed = document.querySelector('.feed');
                 loadFeed(0, () => {
-                initURL = document.querySelector('.entry-link');
+                initURL = feed.querySelector('.entry-link');
                 loadFeed(1, () => {
-                nextURL = document.querySelector('.entry-link');
+                nextURL = feed.querySelector('.entry-link');
                 //From Udacity Classroom: "Callbacks should be used to ensure that feeds are loaded by 
                 //the _loadFeed_ function that the content actually changes."
                 done();
             });
         });
     });
+        /* This test ensures that when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous.
+         */
         it('content  has actually changed', () => {
             expect(initURL === nextURL).toBe(false);
         });
     });
-    });
+});
